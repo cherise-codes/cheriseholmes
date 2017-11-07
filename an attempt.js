d5d5fda3bb84faf329a86c46e79128f15ef864d4
cnh3qa <cnh3qa@virginia.edu>
2017-11-07 17:44:06 -0500

    Add Website Pages
    Add pages for website.

---
+++ an attempt.js
@@ -0,0 +1,34 @@
+
+var poem1S = document.getElementById("poem1").style;
+var poem2S = document.getElementById("poem2").style;
+var prevS = document.getElementById("prevButton").style;
+var nextS = document.getElementById("nextButton").style;
+
+var pg = 1;
+var place = pg + "/2";
+document.getElementById("pageNum").innerHTML = place;
+
+function nextPage() {
+    if (poem1S.display != "none") {
+	poem1S.display = "none";
+	poem2S.display = "inline-block";
+	prevS.visibility= "visible";
+	nextS.visibility = "hidden";
+	pg += 1;
+	document.getElementById("pageNum").innerHTML = pg + "/2";
+	
+    }
+}
+
+
+function prevPage() {
+    if (poem2S.display != "none") {
+	poem2S.display = "none";
+	poem1S.display = "inline-block";
+	nextS.visibility = "visible";
+	prevS.visibility = "hidden";
+	pg -= 1;
+	document.getElementById("pageNum").innerHTML = pg + "/2";
+	
+    }
+}
