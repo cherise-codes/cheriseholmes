d0a849fd80ed35725df4c33b00dcd5a8ea19fc51
cnh3qa <cnh3qa@virginia.edu>
2017-11-12 00:50:06 -0500

    add side nav functions

--- an attempt.js
+++ an attempt.js
@@ -34,6 +34,18 @@ function prevPage() {
     }
 }
 
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
 function makeSmall() {
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 	    contentS.width = 95%;
